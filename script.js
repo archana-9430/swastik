2d05a1d3333c6f60ff48ac2519b33b10cbf0c9e2
archana-9430 <archanakumari943064@gmail.com>
2022-08-26 22:41:08 +0530

    add

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+  
+    // typing text animation script
     
+   //var typed = new Typed(".typing-2", {
+   //    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+   //    typeSpeed: 100,
+   //    backSpeed: 60,
+   //    loop: true
+   //});
+   //// owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
